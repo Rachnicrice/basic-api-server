2ee9a6fe6a7440fcdd5764b256d537a1074d098b
Rachnicrice <46403371+Rachnicrice@users.noreply.github.com>
2021-08-19 13:46:56 -0400

    fixed tests

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -40,70 +40,46 @@ describe('Cat Route Testing', () => {
     isPlottingWorldDomination: true,
   };
   
-  beforeAll(async () => {
+  beforeEach(async () => {
     await db.sync();
   });
 
-  afterAll(async () => {
+  afterEach(async () => {
     await db.drop();
   });
 
   it('should respond with a 404 there is no route found', async () => {
-    await mockRequest.get('/billie_eilish')
-      .then(results => {
-        expect(results.status).toEqual(404);
-      });
+    let results = await mockRequest.get('/billie_eilish');
+    expect(results.status).toEqual(404);
   });
 
   it('should get all the cats', async () => {
-    await mockRequest.post('/cat').send(cat)
-      .then( results => {
-        mockRequest.get('/cats')
-          .then(results => {
-            expect(results.json).toBeDefined();
-            expect(results.status).toEqual(200);
-          });
-      });
+    await mockRequest.post('/cat').send(cat);
+    let getResults = await mockRequest.get('/cats');
+    expect(getResults.status).toEqual(200);
   });
 
   it('should get one cat', async () => {
-    await mockRequest.post('/cat').send(cat)
-      .then( results => {
-        mockRequest.get(`/cat/${results.id}`)
-          .then(results => {
-            expect(results.json).toBeDefined();
-            expect(results.status).toEqual(200);
-          });
-      });
+    let results = await mockRequest.post('/cat').send(cat);
+    let oneCat = await mockRequest.get(`/cat/${results.body.id}`);
+    expect(oneCat.status).toEqual(200);
   });
 
   it('should create a new cat', async () => {
-    await mockRequest.post('/cat').send(cat)
-      .then(results => {
-        expect(results.status).toEqual(201);
-      });
+    let results = await mockRequest.post('/cat').send(cat);
+    expect(results.status).toEqual(201);
   });
 
   it('should update a cat', async () => {
-    await mockRequest.post('/cat').send(cat)
-      .then( results => {
-        mockRequest.put(`/cat/${results.id}`).send({name: 'Mr.Meows-a-lot'})
-          .then(results => {
-            expect(results.json).toBeDefined();
-            expect(results.status).toEqual(202);
-          });
-      });
+    let results = await mockRequest.post('/cat').send(cat);
+    let updateResults = await mockRequest.put(`/cat/${results.body.id}`).send({name: 'Mr.Meows-a-lot'});
+    expect(updateResults.status).toEqual(202);
   });
 
   it('should delete a cat', async () => {
-    await mockRequest.post('/cat').send(cat)
-      .then( results => {
-        mockRequest.delete(`/cat/${results.id}`)
-          .then(results => {
-            expect(results.json).toBeDefined();
-            expect(results.status).toEqual(204);
-          });
-      });
+    let results = await mockRequest.post('/cat').send(cat);
+    let delResults = await mockRequest.delete(`/cat/${results.body.id}`);
+    expect(delResults.status).toEqual(204);
   });
 
 });
@@ -117,57 +93,46 @@ describe('Dog Route Testing', () => {
     isGoodestBoi: true,
   };
 
-  beforeAll(async () => {
+  beforeEach(async () => {
     await db.sync();
-    jest.setTimeout(30000);
   });
 
-  afterAll(async () => {
+  afterEach(async () => {
     await db.drop();
   });
 
   it('should respond with a 404 there is no route found', async () => {
-    await mockRequest.get('/unicorns')
-      .then(results => {
-        expect(results.status).toEqual(404);
-      });
+    let results = await mockRequest.get('/unicorn');
+    expect(results.status).toEqual(404);
   });
 
   it('should get all the dogs', async () => {
-    await mockRequest.post('/dog').send(dog)
-      .then( results => {
-        mockRequest.get('/dogs')
-          .then(results => {
-            expect(results.json).toBeDefined();
-            expect(results.status).toEqual(200);
-          });
-      });
+    await mockRequest.post('/dog').send(dog);
+    let results = await mockRequest.get('/dogs');
+    expect(results.status).toEqual(200);
   });
 
   it('should get one dog', async () => {
-    await mockRequest.post('/dog').send(dog)
-      .then( results => {
-        mockRequest.get(`/dog/${results.id}`)
-          .then(results => {
-            expect(results.json).toBeDefined();
-            expect(results.status).toEqual(200);
-          });
-      });
+    let results = await mockRequest.post('/dog').send(dog);
+    let oneDog = await mockRequest.get(`/dog/${results.body.id}`);
+    expect(oneDog.status).toEqual(200);
   });
 
   it('should create a new dog', async () => {
-    return await mockRequest.post('/dog').send(dog)
-      .then(results => {
-        expect(results.status).toEqual(201);
-      });
+    let results = await mockRequest.post('/dog').send(dog);
+    expect(results.status).toEqual(201);
   });
 
   it('should update a dog', async () => {
-
+    let results = await mockRequest.post('/dog').send(dog);
+    let updateResults = await mockRequest.put(`/dog/${results.body.id}`).send({name: 'Gingerooo'});
+    expect(updateResults.status).toEqual(202);
   });
 
   it('should delete a dog', async () => {
-
+    let results = await mockRequest.post('/dog').send(dog);
+    let delResults = await mockRequest.delete(`/dog/${results.body.id}`);
+    expect(delResults.status).toEqual(204);
   });
 
 });
